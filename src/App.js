648600268922eaf49c096ffe8ece41fca863bee5
Adam Kelly <adamkelly2201@gmail.com>
2018-02-24 19:40:05 +0100

    Add Loader

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import Loadable from 'react-loading-overlay';
+
 import Instruction from './instruction/Instruction.js';
 import UrlForm from './form/Form';
 import './App.css';
+
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       error: false,
-      message: 'Just enter the url of a medium article and we\'ll create a table of contents.'
+      message: 'Just enter the url of a medium article and we\'ll create a table of contents.',
+      loading: false
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -16,6 +20,10 @@ class App extends Component {
 
   handleSubmit(url) {
     console.log(this)
+
+    this.setState({
+      loading: true
+    })
 
     if (url === 'http://url.com') {
       this.setState({
@@ -27,12 +35,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="content">
+        <Loadable active={this.state.loading} spinner color="#00AB6C" background="white" text='Creating Your Table Of Contents...'>
+          <div className="content">
           <h1>Medium TOC</h1>
           <Instruction error={this.state.error} message={this.state.message} />
 
           <UrlForm handleSubmit={this.handleSubmit} />
         </div>
+        </Loadable>
+
+        
       </div>
     );
   }
