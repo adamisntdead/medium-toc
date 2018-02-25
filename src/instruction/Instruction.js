fcf9e9c6aeccb16b241d69d4a4d997c749d204f9
Adam Kelly <adamkelly2201@gmail.com>
2018-02-25 23:35:09 +0000

    Add Extra Help Link

--- src/instruction/Instruction.js
+++ src/instruction/Instruction.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./Instruction.css";
 
+const extraHelp = <a href="https://medium.com/@adamisntdead/how-to-easily-create-a-table-of-contents-for-your-article-507e313b2af3">Get more help.</a>
+
 class Instruction extends Component {
   render() {
     return (
       <div className="instruction">
-        <p className={this.props.error ? "red" : ""}>{this.props.message}</p>
+        <p className={this.props.error ? "red" : ""}>{this.props.message} {(() => this.props.error ? extraHelp : '')()}</p>
       </div>
     );
   }
